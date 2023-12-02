2cee16282c784be686ed56803a2d8670fd27c80d
Oli <59261465+offtian@users.noreply.github.com>
2023-12-02 17:41:25 +0000

    Added logging trail

--- src/streamlit_utils/ui_components.py
+++ src/streamlit_utils/ui_components.py
@@ -1,9 +1,20 @@
+import os
 import streamlit as st
 
 
 def setup_streamlit_page():
     """Configure Streamlit's page settings and initialize the session state for question count."""
-    st.set_page_config(page_title="20 Questions with LLM", page_icon="🧠")
+    if "sbstate" not in st.session_state:
+        st.session_state.sbstate = "collapsed"
+    if not os.getenv("OPENAI_API_KEY"):
+        st.session_state.sbstate = "auto"
+
+    st.set_page_config(
+        page_title="20 Questions with LLM",
+        page_icon="🧠",
+        layout="wide",
+        initial_sidebar_state=st.session_state.sbstate,
+    )
     st.title("🧠 Play 20 Questions with LLM")
     st.divider()
 
@@ -14,10 +25,21 @@ def initialize_question_count():
         st.session_state.question_count = 1
 
 
+def restart():
+    """Restart streamlit app by resetting the question count and rerunning the Streamlit app."""
+    st.session_state.langchain_messages = []
+    st.session_state.question_count = 1
+
+
 def get_temperature_setting():
     """Get the temperature setting from the user using a slider."""
     with st.sidebar:
         temperature = st.slider(
-            "Set LLM Temperature", min_value=0.0, max_value=1.0, value=0.0, step=0.1
+            "Set LLM Temperature",
+            min_value=0.0,
+            max_value=1.0,
+            value=0.0,
+            step=0.1,
+            help="Controls how creativity in AI's response",
         )
     return temperature
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
+import logging
+from datetime import datetime
 import streamlit as st
 from streamlit_feedback import streamlit_feedback
 from langchain.memory import ConversationBufferMemory
@@ -5,12 +7,20 @@ from langchain.memory.chat_message_histories import StreamlitChatMessageHistory
 
 from src.twenty_questions_game import TwentyQuestionsGame
 from src.utils.config import load_env_variables, get_api_credentials
+from src.utils.llm_factory import create_llm
 from src.streamlit_utils.ui_components import (
     setup_streamlit_page,
     initialize_question_count,
     get_temperature_setting,
+    restart,
 )
-from src.utils.llm_factory import create_llm
+
+# Setup logger
+logging.basicConfig(
+    filename="logs/game_logs.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(message)s",
+)
 
 
 class StreamlitChatApp:
@@ -25,7 +35,6 @@ class StreamlitChatApp:
         load_env_variables()
         setup_streamlit_page()
         initialize_question_count()
-        self.setup_memory()
         (
             openai_api_type,
             deployment_name,
@@ -42,6 +51,7 @@ class StreamlitChatApp:
             openai_api_key,
             self.temperature,
         )
+        self.setup_memory()
         self.setup_game()  # Initialize the game logic
 
     def setup_memory(self):
@@ -52,9 +62,9 @@ class StreamlitChatApp:
             chat_memory=self.msgs, memory_key="chat_history", return_messages=True
         )
         if len(self.msgs.messages) == 0:
-            self.msgs.add_ai_message(
-                "Let's play 20 Questions! Think of an object, and I will try to guess it. You can only answer 'Yes' or 'No'."
-            )
+            ai_message = "Let's play 20 Questions! Think of an object, and I will try to guess it. You can only answer 'Yes' or 'No'."
+            self.msgs.add_ai_message(ai_message)
+            self.log_game_data(None, ai_message, user_feedback=None)
 
     def setup_game(self):
         """Initialize the TwentyQuestionsGame with the configured LLM and memory."""
@@ -71,7 +81,12 @@ class StreamlitChatApp:
                 response = self.game.run(user_input)
                 self.handle_game_logic(response)
                 st.chat_message("ai").write(response)
-                streamlit_feedback(feedback_type="thumbs")
+                self.log_game_data(
+                    user_input,
+                    response,
+                    user_feedback=streamlit_feedback(feedback_type="thumbs"),
+                )
+
             except Exception as e:
                 st.error(f"An error occurred: {e}")
 
@@ -85,7 +100,7 @@ class StreamlitChatApp:
                 f"AI successfully guessed the object correctly in {st.session_state.question_count} questions!"
             )
 
-            return st.button("Restart Game", on_click=self.restart_game)
+            return st.button("Restart Game", on_click=restart)
 
         st.session_state.question_count += 1
 
@@ -95,14 +110,23 @@ class StreamlitChatApp:
             )
 
             st.error("Game over! The AI failed to guess in 20 questions.")
-            st.button("Restart Game", on_click=self.restart_game)
+            st.button("Restart Game", on_click=restart)
             st.stop()
 
-    def restart_game(self):
-        """Restart the game by resetting the question count and rerunning the Streamlit app."""
-        st.session_state.langchain_messages = []
-        st.session_state.question_count = 1
-        # st.rerun()
+    def log_game_data(self, user_input, ai_response, user_feedback):
+        """Log game data including chat history and other details."""
+        log_entry = {
+            "timestamp": datetime.now().isoformat(),
+            "ai_response": ai_response,
+            "user_input": user_input,
+            "temperature": self.temperature,
+            "llm_details": {
+                "api_type": self.llm.openai_api_type,
+                "deployment_name": "DEPLOYMENT_NAME",  # Anonymised
+            },
+            "user_feedback": user_feedback,
+        }
+        logging.info(log_entry)
 
 
 # Main execution
